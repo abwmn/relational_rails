75c73cfedcd21f40b83d453a3f614c2486b04693
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-27 19:56:08 -0400

    feat: story 1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/worlds', to: 'worlds#index'
+  get '/cities', to: 'cities#index'
 end
