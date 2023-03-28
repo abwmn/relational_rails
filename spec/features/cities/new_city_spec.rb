67788d1bd331a6ffe281a7934e1034bc79031a87
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-28 04:27:04 -0400

    test: new worlds and cities

---
+++ spec/features/cities/new_city_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'New City', type: :feature do
+  describe 'As a visitor' do
+    it 'I can create a new city' do
+      world = World.create!(name: 'Earth', number_of_continents: 7, climate: 'Temperate', inhabitable: true)
+      
+      visit cities_path
+      click_link 'New City'
+
+      expect(current_path).to eq(new_city_path)
+
+      fill_in 'Name', with: 'New York'
+      select 'Earth', from: 'World'
+      fill_in 'Population', with: 8500000
+      fill_in 'Technological level', with: 8
+      check 'Inhabited'
+
+      click_button 'Create City'
+
+      expect(current_path).to eq(cities_path)
+      expect(page).to have_content('New York')
+    end
+  end
+end
---
+++ spec/features/worlds/new_world_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'New World', type: :feature do
+  describe 'As a visitor' do
+    it 'I can create a new world' do
+      
+      visit worlds_path
+      click_link 'New World'
+
+      expect(current_path).to eq(new_world_path)
+
+      fill_in 'Name', with: 'Venus'
+      fill_in 'Number of continents', with: 0
+      fill_in 'Climate', with: 'gaseous'
+      
+
+      click_button 'World'
+
+      expect(current_path).to eq(worlds_path)
+      expect(page).to have_content('Venus')
+    end
+  end
+end
