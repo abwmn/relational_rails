b8da5ad4dc247aab1e9b958dcbbbb13a862db754
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-27 23:47:39 -0400

    test: inhabitant show

--- spec/features/inhabitant_show_spec.rb
+++ spec/features/inhabitant_show_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe 'User Story 4, Inhabitant Show' do
   context 'As a visitor' do
     it "I see the child with that id including the child's attributes" do
-      city = City.create!(name: "New York City", population: 8_000_000, technological_level: 8, inhabited: true)
+      world = World.create!(name: "Earth", number_of_continents: 7, climate: "diverse", inhabitable: true)
+      city = City.create!(name: "New York City", population: 8_000_000, technological_level: 8, inhabited: true, world_id: world.id)
       inhabitant = Inhabitant.create!(name: "John Doe", age: 30, married: false, role: "Engineer", city_id: city.id)
 
       visit "/inhabitants/#{inhabitant.id}"
