eb463324bbfbd10ca66a8c75467fb45a815b404d
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-27 22:33:11 -0400

    test: world and city models

--- spec/features/worlds/cities/index_spec.rb
+++ spec/features/worlds/cities/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'User Story 1, City Index' do
   context 'As a visitor' do
     it 'I see the name of each city in the system' do
-      world = World.create!(name: "Earth", number_of_continents: 7, climate: "diverse", inhabitable: true)
+      world = World.create!(name: "Earth", age_in_m: 4_000, climate: "diverse", inhabitable: true)
       city1 = City.create!(name: "New York City", population: 8_000_000, technological_level: 8, inhabited: true, world_id: world.id)
       city2 = City.create!(name: "Paris", population: 7_000_000, technological_level: 8, inhabited: true, world_id: world.id)
       
--- spec/features/worlds/index_spec.rb
+++ spec/features/worlds/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'User Story 1, World Index' do
   context 'As a visitor' do
     it 'I see the name of each world in the system' do
-      world = World.create!(name: "Earth", number_of_continents: 7, climate: "diverse", inhabitable: true)
-      world = World.create!(name: "Mars", number_of_continents: 1, climate: "barren", inhabitable: false)
+      world1 = World.create!(name: "Earth", number_of_continents: 7, climate: "diverse", inhabitable: true)
+      world2 = World.create!(name: "Mars", number_of_continents: 1, climate: "barren", inhabitable: false)
 
       visit '/worlds'
       expect(page).to have_content(world1.name)
