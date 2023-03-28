7995382359721cbc48a5886813d31c7737da2460
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-28 04:26:24 -0400

    user story 11: create worlds and cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,6 +3,20 @@ class CitiesController < ApplicationController
     @cities = City.order(created_at: :desc)
   end
 
+  def new
+    @city = City.new
+  end
+  
+  def create
+    @city = City.new(city_params)
+    
+    if @city.save
+      redirect_to cities_path
+    else
+      render :new
+    end
+  end
+
   def show
     @city = City.find(params[:id])
   end
@@ -10,5 +24,11 @@ class CitiesController < ApplicationController
   def world_cities
     @world = World.find(params[:world_id])
     @cities = @world.cities
+  end
+
+  private
+  
+  def city_params
+    params.require(:city).permit(:name, :world_id, :population, :technological_level, :inhabited)
   end
 end
--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -3,7 +3,27 @@ class WorldsController < ApplicationController
     @worlds = World.order(created_at: :desc)
   end
 
+  def new
+    @world = World.new
+  end
+  
+  def create
+    @world = World.new(world_params)
+    
+    if @world.save
+      redirect_to worlds_path
+    else
+      render :new
+    end
+  end
+
   def show
     @world = World.find(params[:id])
+  end
+
+  private
+  
+  def world_params
+    params.require(:world).permit(:name, :number_of_continents, :climate, :inhabitable)
   end
 end
